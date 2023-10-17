d929e74e39d0f94e372dbb812805eb71b67f9ee3
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-10-17 11:44:04 +0200

    handle internet identity canister url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,32 @@
-import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+import canisterIds from './.dfx/local/canister_ids.json'
 
-// https://astro.build/config
+const isDev = process.env.NODE_ENV === 'development'
+
+const baseIdentityUrlDev = 'http://127.0.0.1:8000/?canisterId='
+const baseIdentityUrlProd = 'https://identity.ic0.app'
+
+let finalIdentityUrl = baseIdentityUrlProd
+if (isDev) {
+    const iiCanisterId = canisterIds.internet_identity.local
+    console.log('Using local Internet Identity canister ID: ' + iiCanisterId)
+    finalIdentityUrl = baseIdentityUrlDev + iiCanisterId
+    console.log('Using local Internet Identity URL: ' + finalIdentityUrl)
+}
+
 export default defineConfig({
+    vite: {
+        server: {
+            watch: {
+                ignored: '**/.dfx/**',
+            },
+        },
+        define: {
+            'import.meta.env.PUBLIC_II_URL': JSON.stringify(finalIdentityUrl),
+        },
+    },
     integrations: [
         react(),
         tailwind({
