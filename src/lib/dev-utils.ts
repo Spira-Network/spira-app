fd3dc6210c7ee3d6921bdb094673365bbe578558
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-10-17 02:35:11 +0200

    fix minor issues causing console errors sometimes

--- src/lib/dev-utils.ts
+++ src/lib/dev-utils.ts
@@ -3,20 +3,22 @@ let scrollPosition: number = 0;
 if (import.meta.hot) {
   const main = document.querySelector("main");
 
-  import.meta.hot.on("vite:beforeFullReload", () => {
-    scrollPosition = main.scrollTop;
-    localStorage.setItem("scrollPosition", scrollPosition.toString());
-  });
+  if (main) {
+    import.meta.hot.on("vite:beforeFullReload", () => {
+      scrollPosition = main.scrollTop;
+      localStorage.setItem("scrollPosition", scrollPosition.toString());
+    });
 
-  import.meta.hot.on("vite:ws:connect", () => {
-    const savedScroll = localStorage.getItem("scrollPosition");
-    if (savedScroll) {
-      scrollPosition = parseInt(savedScroll);
+    import.meta.hot.on("vite:ws:connect", () => {
+      const savedScroll = localStorage.getItem("scrollPosition");
+      if (savedScroll) {
+        scrollPosition = parseInt(savedScroll);
 
-      const originalScrollBehavior = main.style.scrollBehavior;
-      main.style.scrollBehavior = "auto";
-      main.scrollTo(0, scrollPosition);
-      main.style.scrollBehavior = originalScrollBehavior;
-    }
-  });
+        const originalScrollBehavior = main.style.scrollBehavior;
+        main.style.scrollBehavior = "auto";
+        main.scrollTo(0, scrollPosition);
+        main.style.scrollBehavior = originalScrollBehavior;
+      }
+    });
+  }
 }
