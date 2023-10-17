b1b7f1f05fb7e879f5dbe1608bd192c2b467b06c
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-10-17 02:42:39 +0200

    prepare form field components

---
+++ src/components/labeled-input.tsx
@@ -0,0 +1,20 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function LabeledInput({ id, label, placeholder, type }) {
+    return (
+      <div className="flex flex-col gap-4 w-full">
+        <Label htmlFor={id} className="text-left font-robotoslab text-[#555]">
+          {label}
+        </Label>
+        <Input
+          id={id}
+          placeholder={placeholder}
+          type={type}
+        className="p-6 rounded-sm placeholder:text-[#adadad]"
+        autoComplete="off"
+        />
+      </div>
+    );
+  }
+  
