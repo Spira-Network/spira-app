b3ad3ffbd09e6061b1123a72b99ed585a5526e8a
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-10-18 10:07:09 +0200

    apply minor avatar style changes

--- src/components/connected-avatar.tsx
+++ src/components/connected-avatar.tsx
@@ -5,13 +5,13 @@ export default function ConnectedAvatar({ onDisconnect }) {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Avatar className='w-8 h-8 -mr-6'>
-                    <AvatarImage src='https://github.com/memo.png' alt='@shadcn' />
-                    <AvatarFallback>CN</AvatarFallback>
+                <Avatar className='w-8 h-8 -mr-6 border-black border-2'>
+                    <AvatarImage src='https://github.com/memo.png' alt='avatar' />
+                    <AvatarFallback>IC</AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align='center' asChild>
-                <ul className='w-32 flex flex-col p-4 mt-4 border-t-4 rounded-none backdrop-filter bg-slate-800 bg-opacity-40 backdrop-blur-md shadow-md border-slate-600'>
+                <ul className='box-border w-32 flex flex-col p-4 mt-4 border-t-4 rounded-none backdrop-filter bg-slate-800 bg-opacity-40 backdrop-blur-md shadow-md border-slate-600'>
                     <DropdownMenuItem asChild>
                         <li onClick={onDisconnect} className='text-white text-base font-montserrat font-semibold'>
                             Disconnect
