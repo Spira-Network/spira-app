3be3c9bdff4c7dba1a6db354a62a6544d8b8cf1b
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-10-19 12:07:36 +0200

    configure eslint import plugins and rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,10 +16,36 @@ module.exports = {
         'plugin:astro/recommended',
         'plugin:astro/jsx-a11y-recommended',
         'plugin:tailwindcss/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
         'prettier',
     ],
-    plugins: ['preferred-import'],
-    rules: { 'preferred-import/ts-imports': 'error' },
+    plugins: ['unused-imports', 'simple-import-sort', 'preferred-import'],
+    settings: {
+        'import/core-modules': ['astro'],
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true,
+                project,
+            },
+        },
+    },
+    rules: {
+        'import/first': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+        // 'import/no-unresolved': ['error', { ignore: ['^astro'] }],
+        'import/order': 'off', // handled by simple-import-sort/imports
+        'no-unused-vars': 'off', // handled by unused-imports/no-unused-vars
+        'preferred-import/ts-imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'simple-import-sort/imports': 'error',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+        ],
+    },
     overrides: [
         {
             files: ['*.astro'],
