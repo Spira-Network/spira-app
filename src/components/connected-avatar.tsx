9ab36aee012f66988336cc03ab6b3c0844239b33
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-11-06 03:33:03 +0100

    use local assets for current mockups

--- src/components/connected-avatar.tsx
+++ src/components/connected-avatar.tsx
@@ -9,7 +9,7 @@ export default function ConnectedAvatar({ onDisconnect }) {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Avatar className='-mr-6 h-8 w-8 border-2 border-black' role='img' aria-labelledby='avatar-username'>
-                    <AvatarImage src='https://github.com/memo.png' alt='avatar' />
+                    <AvatarImage src='/profile_avatar.jpg' alt='avatar' />
                     <AvatarFallback aria-hidden='true' delayMs={0}>
                         <Skeleton className='h-full w-full bg-slate-500'></Skeleton>
                     </AvatarFallback>
--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 export default function UserAvatar({ className = '' }) {
     return (
         <Avatar className={cn('w-32 h-32 drop-shadow-lg', className)}>
-            <AvatarImage src='https://github.com/egypt.png' alt='avatar' />
+            <AvatarImage src='/profile_avatar.jpg' alt='avatar' />
             <AvatarFallback>AV</AvatarFallback>
         </Avatar>
     )
