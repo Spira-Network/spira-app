9470e0fcea6a42168ae41470f379fbecff7aa6a3
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-11-12 02:48:28 +0100

    map prototype

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="astro/client" />
 
+import 'mapbox-gl'
+
 interface Document {
     startViewTransition
+}
+
+declare module 'mapbox-gl' {
+    interface Map {
+        setConfigProperty(importId: string, configName: string, value: unknown): this
+    }
 }
