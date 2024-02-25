1bffb8d0a5038b7c195e4a13fe26ee251adc1e01
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-02-25 13:49:34 -0700

    Update Astro configuration and environment variable handling

--- tools/astro-ii-integration/index.ts
+++ tools/astro-ii-integration/index.ts
@@ -1,12 +1,15 @@
+import fs from 'node:fs'
 import { createRequire } from 'node:module'
 import path from 'node:path'
 
 import type { AstroIntegration, AstroIntegrationLogger } from 'astro'
+import { globSync } from 'glob'
+
 // import { bgMagenta, black, dim, magenta, yellow } from 'kleur'
 
 const require = createRequire(import.meta.url)
 const BASE_URLS = {
-    DEV: 'http://localhost:8000/?canisterId=',
+    DEV: 'http://br5f7-7uaaa-aaaaa-qaaca-cai.localhost:8000',
     PROD: 'https://identity.ic0.app',
 }
 
@@ -29,6 +32,8 @@ export default (): AstroIntegration => ({
                     },
                 },
             })
+
+            replaceEnvVariables(logger)
         },
     },
 })
@@ -40,7 +45,8 @@ function getInternetIdentityUrl(isDev: boolean, logger: AstroIntegrationLogger):
             const canisterIds = require(CANISTER_IDS_PATH)
             const iiCanisterId = canisterIds?.internet_identity?.local
             if (iiCanisterId) {
-                url = `${BASE_URLS.DEV}${iiCanisterId}`
+                url = `${BASE_URLS.DEV}`
+                // url = `${BASE_URLS.DEV}${iiCanisterId}`
             }
         } catch {
             logger.error('File canister_ids.json not found, using production internet identity.')
@@ -64,4 +70,59 @@ function logIdentityDetails(url: string, env: string) {
     ].join('\n')
 
     console.log(logs)
+}
+
+function replaceEnvVariables(logger: AstroIntegrationLogger) {
+    // Load and parse .env file
+    const envPath = path.join(process.cwd(), '.env')
+    const envFileContent = fs.readFileSync(envPath, { encoding: 'utf-8' })
+    const envVariables = parseEnvFile(envFileContent)
+
+    // Define the pattern for files to process
+    const filesPattern = './src/declarations/*/index.js'
+
+    const files = globSync(filesPattern)
+
+    if (files.length === 0) {
+        logger.error(`No declarations found with pattern ${filesPattern}`)
+        throw new Error('No declarations found, please run `dfx generate` first.')
+    }
+
+    files.forEach(file => {
+        let content = fs.readFileSync(file, 'utf8')
+
+        // Replace each environment variable in the file content
+        Object.entries(envVariables).forEach(([key, value]) => {
+            const regex = new RegExp(`process.env.${key}\\b`, 'g')
+            content = content.replace(regex, `${value}`)
+        })
+
+        fs.writeFileSync(file, content, 'utf8')
+        logger.info(`Updated environment variables in ${file}`)
+    })
+}
+
+function parseEnvFile(content: string) {
+    const lines = content.split('\n')
+    const envVariables = {}
+    let isEnvSection = false
+
+    lines.forEach(line => {
+        if (line.trim() === '# DFX CANISTER ENVIRONMENT VARIABLES') {
+            isEnvSection = true
+            return
+        }
+
+        if (line.trim() === '# END DFX CANISTER ENVIRONMENT VARIABLES') {
+            isEnvSection = false
+        }
+
+        // Match only non-comment lines and lines between the environment variable section
+        if (isEnvSection && line.trim() && !line.startsWith('#')) {
+            const [key, value] = line.split('=')
+            envVariables[key.trim()] = value.trim()
+        }
+    })
+
+    return envVariables
 }
